0211c682bf19c17918a5ebae6a1819c4e9cb9b0e
TheMineWay <joelcamposoliva9000@gmail.com>
2024-04-20 14:51:58 +0200

    addViking & addSaxon & vikingAttack

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -42,16 +42,25 @@ class Saxon(Soldier):
 
 class War():
     def __init__(self):
-        # your code here
+        self.vikingArmy: list[Viking] = []
+        self.saxonArmy: list[Saxon] = []
 
     def addViking(self, viking):
-        # your code here
+        self.vikingArmy.append(viking)
     
     def addSaxon(self, saxon):
-        # your code here
+        self.saxonArmy.append(saxon)
     
     def vikingAttack(self):
-        # your code here
+        if len(self.saxonArmy) <= 0 or len(self.saxonArmy) <= 0:
+            return None
+
+        victim = self.saxonArmy[random.randrange(0, len(self.saxonArmy))]
+        attacker = self.vikingArmy[random.randrange(0, len(self.vikingArmy))]
+        res = victim.receiveDamage(attacker.strength)
+
+        self.saxonArmy = [saxon for saxon in self.saxonArmy if saxon.health > 0]
+        return res
     
     def saxonAttack(self):
         # your code here
